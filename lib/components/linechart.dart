80f55dbfd8f35cfeafb6f768de041ca227e5603a
SIDDHANT SARAF <siddhantsaraf.91@gmail.com>
2021-06-08 05:11:54 +0530

    fl_chart lib used insted of flutter_chart
    added linechart

--- lib/components/linechart.dart
+++ lib/components/linechart.dart
@@ -1,103 +1,124 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:fl_chart/fl_chart.dart';
 import 'package:phygitalz_standard/components/linechartbuilder.dart';
-import 'charts.dart';
-class CustomLChart extends StatelessWidget {
-  final List<Datalist> data;
+import 'dart:math';
 
-  const CustomLChart({Key key,this.data}) : super(key: key);
+class Datalist {
+  final int date;
+  final int breakdown;
+  final int incidents;
 
+  Datalist({
+    @required this.date,
+    @required this.breakdown,
+    this.incidents,
+  });
+}
+
+class CustomLChart extends StatefulWidget {
+  //List<Datalist> data;
+  // map.entries.forEach((e) => list.add(Datalist(e.key, e.value)));
+  // List<Datalist>data = Map.entries.map((e) => Datalist(e.date, e.breakdown)).toList();
+  // //print(list);
+  CustomLChart({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _CustomLChartState createState() => _CustomLChartState();
+}
+
+class _CustomLChartState extends State<CustomLChart> {
+  final List<FlSpot> dummyData1 = List.generate(15, (index) {
+    return FlSpot(index.toDouble(), index * Random().nextDouble());
+  });
+  //List<Datalist> data ;
+  final List<Color> _gradientColors = [
+    const Color(0xFF6FFF7C),
+    const Color(0xFF0087FF),
+    const Color(0xFF5620FF),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    List<charts.Series<Datalist, int>> series = [
-      charts.Series(
-          data: data,
-          id: "DataConsumption",
-          domainFn: (Datalist series, _) => series.date,
-          measureFn: (Datalist series, _) => series.breakdown,
-          colorFn: (Datalist series, _) => series.barColor,
-
-          labelAccessorFn: (Datalist series, _) =>
-          '${series.breakdown.toString()}'),
-
-    ];
-
     return Container(
-      //decoration: BoxDecoration(),
-      height: 166,
-      padding: EdgeInsets.only(left: 0, right: 0, bottom: 0),
+      height: 160,
       child: Card(
-        //shape: RoundedRectangleBorder(side: BorderSide.none),
         elevation: 8,
         borderOnForeground: true,
         child: Column(
-          children: <Widget>[
+          children: [
             Expanded(
-              child: charts.LineChart(
-                series,
-                animate: true,
-
-                //Asign a Custom style for the domain axis
-                domainAxis: new charts.NumericAxisSpec(
-
-                  //showAxisLine: true,
-                  renderSpec: new charts.SmallTickRendererSpec(
-                    minimumPaddingBetweenLabelsPx: 5,
-                    // Tick and Label styling here.
-                    labelStyle: new charts.TextStyleSpec(
-                        fontSize: 6, // size in Pts.
-                        color: charts.MaterialPalette.black),
-
-                    // Change the line colors to match text color.
-                    lineStyle: new charts.LineStyleSpec(
-                        color: charts.MaterialPalette.black),
-                  ),
-                ),
-
-                /// Assign a custom style for the measure axis.
-                primaryMeasureAxis: new charts.NumericAxisSpec(
-                  // show static tic at value o,50,and 100
-                  tickProviderSpec: new charts.StaticNumericTickProviderSpec(
-                    <charts.TickSpec<num>>[
-                      charts.TickSpec<num>(0),
-                     // charts.TickSpec<num>(50),
-                      //charts.TickSpec<num>(100),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: LineChart(
+                  LineChartData(
+                    extraLinesData: ExtraLinesData(
+                      extraLinesOnTop: true,
+                      horizontalLines: [HorizontalLine(
+                        y: 5,
+                        color: const Color.fromRGBO(197, 210, 214, 1),
+                        strokeWidth: 2,
+                        dashArray: [5, 10],
+                        label: HorizontalLineLabel(
+                          show: true,
+                          alignment: Alignment.topRight,
+                          padding: const EdgeInsets.only(right: 5, bottom: 5),
+                          style: const TextStyle(color: Colors.black, fontSize: 9),
+                          labelResolver: (line) => 'H: ${line.y}',
+                        ),
+                      ),],
+                    ),
+                    borderData: FlBorderData(show: false),
+                    gridData: FlGridData(
+                      show: true,
+                      drawVerticalLine: true,
+                      getDrawingHorizontalLine: (value) {
+                        return FlLine(
+                          color: const Color(0xFFFFFFFF),
+                          strokeWidth: 1,
+                        );
+                      },
+                      getDrawingVerticalLine: (value) {
+                        return FlLine(
+                          color: const Color(0xFFFFFFFF),
+                          strokeWidth: 1,
+                        );
+                      },
+                    ),
+                    // rangeAnnotations: RangeAnnotations(
+                    //   horizontalRangeAnnotations: ,
+                    // ),
+                    lineBarsData: [
+                      LineChartBarData(
+                          spots: dummyData1,
+                          colors: _gradientColors,
+                          colorStops: const [0.25, 0.5, 0.75],
+                          gradientFrom: const Offset(0.5, 0),
+                          gradientTo: const Offset(0.5, 1),
+                          barWidth: 2,
+                          isStrokeCapRound: true,
+                          dotData: FlDotData(show: false),
+                          belowBarData: BarAreaData(
+                            show: true,
+                            colors: _gradientColors
+                                .map((color) => color.withOpacity(0.3))
+                                .toList(),
+                            gradientColorStops: const [0.25, 0.5, 0.75],
+                            gradientFrom: const Offset(0.5, 0),
+                            gradientTo: const Offset(0.5, 1),
+                          ),
+                          //dashArray: [2,6],
+                          isCurved: true,
+                          curveSmoothness: 0.1),
                     ],
                   ),
-                  renderSpec: new charts.GridlineRendererSpec(
-                    // Tick and Label styling here.
-                    labelStyle: new charts.TextStyleSpec(
-                        fontSize: 6, // size in Pts.
-                        color: charts.MaterialPalette.black),
-
-                    // Change the line colors to match text color.
-                    lineStyle: new charts.LineStyleSpec(
-                        color: charts.MaterialPalette.black),
-                  ),
                 ),
-
-                behaviors: [
-
-                  new charts.ChartTitle('Bottom title text',
-                      titleStyleSpec: charts.TextStyleSpec(fontSize: 8),
-                      behaviorPosition: charts.BehaviorPosition.bottom,
-                      titleOutsideJustification:
-                      charts.OutsideJustification.middleDrawArea),
-                  new charts.ChartTitle('Start title',
-                      titleStyleSpec: charts.TextStyleSpec(fontSize: 8),
-                      behaviorPosition: charts.BehaviorPosition.start,
-                      titleOutsideJustification:
-                      charts.OutsideJustification.middleDrawArea),
-                ],
-                defaultRenderer:
-                new charts.LineRendererConfig(includeArea: true,includePoints: true),
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
-  }
-
+}
--- lib/components/linechartbuilder.dart
+++ lib/components/linechartbuilder.dart
@@ -1,79 +1,103 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:fl_chart/fl_chart.dart' as charts;
 import 'package:flutter/foundation.dart';
-import 'linechart.dart';
-
+import 'package:phygitalz_standard/components/linechart.dart';
 class Datalist {
   final int date;
   final int breakdown;
   final int incidents;
-  final charts.Color barColor;
-  final charts.Color barColor2;
 
   Datalist(
       {@required this.date,
         @required this.breakdown,
-        @required this.barColor,
         this.incidents,
-        this.barColor2});
+       });
 }
 
 class lineChart extends StatelessWidget {
   final List<Datalist> data2 = [
     Datalist(
-      date: 25,
+      date: 19,
       breakdown: 71,
       incidents: 5,
-      barColor2: charts.ColorUtil.fromDartColor(Colors.purple),
-      barColor: charts.ColorUtil.fromDartColor(Colors.pink.shade700),
+
+    ),
+    Datalist(
+      date: 20,
+      breakdown: 48,
+      incidents: 6,
+
+    ),
+    Datalist(
+      date: 21,
+      breakdown: 56,
+      incidents: 8,
+
+    ),
+    Datalist(
+      date: 22,
+      breakdown: 35,
+      incidents: 9,
+
+    ),
+    Datalist(
+      date: 23,
+      breakdown: 90,
+      incidents: 15,
+
+    ),
+    Datalist(
+      date: 24,
+      breakdown: 67,
+      incidents: 9,
+
+    ),
+    Datalist(
+      date: 25,
+      breakdown: 56,
+      incidents: 6,
+
     ),
     Datalist(
       date: 26,
-      breakdown: 1,
-      incidents: 6,
-      barColor2: charts.ColorUtil.fromDartColor(Colors.purple),
-      barColor: charts.ColorUtil.fromDartColor(Colors.pink.shade700),
+      breakdown: 71,
+      incidents: 5,
+
     ),
     Datalist(
       date: 27,
-      breakdown: 15,
-      incidents: 0,
-      barColor2: charts.ColorUtil.fromDartColor(Colors.purple),
-      barColor: charts.ColorUtil.fromDartColor(Colors.pink.shade700),
+      breakdown: 48,
+      incidents: 6,
+
     ),
     Datalist(
       date: 28,
-      breakdown: 10,
-      incidents: 9,
-      barColor2:charts.ColorUtil.fromDartColor(Colors.purple),
-      barColor: charts.ColorUtil.fromDartColor(Colors.pink.shade700),
+      breakdown: 56,
+      incidents: 8,
+
     ),
     Datalist(
       date: 29,
-      breakdown: 90,
-      incidents: 15,
-      barColor2: charts.ColorUtil.fromDartColor(Colors.purple),
-      barColor: charts.ColorUtil.fromDartColor(Colors.pink.shade700),
+      breakdown: 35,
+      incidents: 9,
+
     ),
     Datalist(
       date: 30,
-      breakdown: 20,
-      incidents: 0,
-      barColor2: charts.ColorUtil.fromDartColor(Colors.purple),
-      barColor: charts.ColorUtil.fromDartColor(Colors.pink.shade700),
+      breakdown: 90,
+      incidents: 15,
+
     ),
     Datalist(
       date: 31,
-      breakdown: 20,
-      incidents: 6,
-      barColor2: charts.ColorUtil.fromDartColor(Colors.purple),
-      barColor: charts.ColorUtil.fromDartColor(Colors.pink.shade700),
+      breakdown: 67,
+      incidents: 9,
+
     ),
+
   ];
   @override
   Widget build(BuildContext context) {
-    return CustomLChart(
-      data: data2,
-    );
+    return CustomLChart();
   }
 }
